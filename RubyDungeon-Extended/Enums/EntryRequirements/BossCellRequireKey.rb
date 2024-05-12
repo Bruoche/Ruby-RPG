afda8ff4515014685d5ef3cc189cfccdca193960
Alice Mailhes <armand.mailhes@gmail.com>
2024-05-12 15:29:23 +0200

    Adding small fix to bossCell requirement

--- RubyDungeon-Extended/Enums/EntryRequirements/BossCellRequireKey.rb
+++ RubyDungeon-Extended/Enums/EntryRequirements/BossCellRequireKey.rb
@@ -2,15 +2,15 @@ module BossCellRequireKey
     ENTRY_QUESTION = "Essayer la clef de la prison sur la serrure ?"
     ENTRY_MESSAGE = [
         "Vous utilisez la clef de la prison afin d'ouvrir la grande porte.",
-        "Vous poussez sur la porte, ses gonds rouillé vous donnant une résistance forte.",
-        "En y mettant toutes vos forces, vous parvenez à forcer la porte ouverte.",
+        "Vous poussez sur la porte, ses gonds rouillés vous donnant du retord.",
+        "En y mettant toutes vos forces, vous parvenez néanmoins à forcer la porte ouverte.",
         "",
         "Lorsque vous pénétrez dans ce qui semble être une cellule de prison,",
-        "Vous pouvez voir au fond de la salle obscure un chevalier à l'armure endomagé par les batailles et l'age",
+        "Vous pouvez voir au fond de la salle obscure un chevalier à l'armure endomagée par les batailles et l'age",
         "A peine couverte par les fins lambeau de ce qui devait être un noble surcôt."
     ]
     NO_ENTRY_MESSAGE = [
-        "Vous décidez qu'il ne vaut probablement mieux pas prendre le risque d'ouvrir la porte",
+        "Vous décidez qu'il ne vaut probablement mieux ne pas prendre le risque d'ouvrir la porte",
         "Et décidez donc de revenir sur vos pas."
     ]
     IMPOSSIBLE_ENTRY_MESSAGE = [
