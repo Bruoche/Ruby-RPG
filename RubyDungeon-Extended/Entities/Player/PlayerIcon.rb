569e16ae679d0ee50712021d07c5d4fe8c49d3e0
Alice Mailhes <armand.mailhes@gmail.com>
2024-11-27 19:11:43 +0100

    Adding english locale to character creation

--- RubyDungeon-Extended/Entities/Player/PlayerIcon.rb
+++ RubyDungeon-Extended/Entities/Player/PlayerIcon.rb
@@ -15,149 +15,149 @@ class PlayerIcon
     FRONT_FOLDER = 'Front/'
     BACK_FOLDER = 'Back/'
     TONES = {
-        'pale': 'Light/',
-        'mate': 'Half/',
-        'noire': 'Dark/'
+        Locale::KEY_ICON_PALE => 'Light/',
+        Locale::KEY_ICON_HALF => 'Half/',
+        Locale::KEY_ICON_BLACK => 'Dark/'
     }
     SIZES = {
-        'maigre': 'thin',
-        'moyenne': 'medium',
-        'large': 'large'
+        Locale::KEY_ICON_THIN => 'thin',
+        Locale::KEY_ICON_MEDIUM => 'medium',
+        Locale::KEY_ICON_LARGE => 'large'
     }
     EYES_COLOR = {
-        'blanc': 'light',
-        'clair': 'half',
-        'sombre': 'dark'
+        Locale::KEY_ICON_WHITE => 'light',
+        Locale::KEY_ICON_LIGHT => 'half',
+        Locale::KEY_ICON_DARK => 'dark'
     }
     EARS = {
-        'moyennes': 'ears1',
-        'rondes': 'ears2',
-        'larges': 'ears3',
-        'plates': 'ears4'
+        Locale::KEY_ICON_AVERAGES => 'ears1',
+        Locale::KEY_ICON_ROUNDS => 'ears2',
+        Locale::KEY_ICON_LARGES => 'ears3',
+        Locale::KEY_ICON_FLATS => 'ears4'
     }
     NOSES = {
-        'petit rond': 'nose2',
-        'fin': 'nose4',
-        'moyen': 'nose1',
-        'large': 'nose3'
+        Locale::KEY_ICON_SMALL_ROUND_M => 'nose2',
+        Locale::KEY_ICON_NARROW_M => 'nose4',
+        Locale::KEY_ICON_MEDIUM_M => 'nose1',
+        Locale::KEY_ICON_LARGE_M => 'nose3'
     }
     BROWS = {
-        'aucun': 'brows6',
-        'ronds': 'brows5',
-        'minis': 'brows4',
-        'moyens': 'brows1',
-        'larges': 'brows2',
-        'mono-sourcil': 'brows3'
+        Locale::KEY_ICON_NONE_M => 'brows6',
+        Locale::KEY_ICON_ROUNDS_M => 'brows5',
+        Locale::KEY_ICON_MINIS_M => 'brows4',
+        Locale::KEY_ICON_MEDIUMS_M => 'brows1',
+        Locale::KEY_ICON_LARGES_M => 'brows2',
+        Locale::KEY_ICON_UNIBROW => 'brows3'
     }
     BEARDS = {
-        'aucune': 'None/',
-        'barbiche': 'Beard1/',
-        'mini-bouc': 'Beard2/',
-        'bouc': 'Beard3/',
-        'bouc fin': 'Beard4/',
-        'bouc en pointe': 'Beard5/',
-        'bouc long': 'Beard6/',
-        'bouc large': 'Beard7/',
-        'petite barbe': 'Beard8/',
-        'barbe': 'Beard9/',
-        'barbe longue': 'Beard10/'
+        Locale::KEY_ICON_NONE => 'None/',
+        Locale::KEY_ICON_BARBICHE => 'Beard1/',
+        Locale::KEY_ICON_MINIBOUC => 'Beard2/',
+        Locale::KEY_ICON_BOUC => 'Beard3/',
+        Locale::KEY_ICON_THIN_BOUC => 'Beard4/',
+        Locale::KEY_ICON_POINTY_BOUC => 'Beard5/',
+        Locale::KEY_ICON_LONG_BOUC => 'Beard6/',
+        Locale::KEY_ICON_LARGE_BOUC => 'Beard7/',
+        Locale::KEY_ICON_SMALL_BEARD => 'Beard8/',
+        Locale::KEY_ICON_BEARD => 'Beard9/',
+        Locale::KEY_ICON_LONG_BEARD => 'Beard10/'
     }
     MOUSTACHES = {
-        'aucune': 'None/',
-        'fine': 'Moustache1/',
-        'broussaille': 'Moustache2/',
-        'fer à cheval': 'Moustache3/',
-        'Fu Manchu': 'Moustache4/',
-        'brosse': 'Moustache5/',
-        'crayon': 'Moustache6/',
-        'française': 'Moustache7/',
-        'crochet': 'Moustache8/',
-        'guidon': 'Moustache9/'
+        Locale::KEY_ICON_NONE => 'None/',
+        Locale::KEY_ICON_NARROW => 'Moustache1/',
+        Locale::KEY_ICON_BUSH => 'Moustache2/',
+        Locale::KEY_ICON_HORSESHOE => 'Moustache3/',
+        Locale::KEY_ICON_FUMANCHU => 'Moustache4/',
+        Locale::KEY_ICON_BRUSH => 'Moustache5/',
+        Locale::KEY_ICON_CRAYON => 'Moustache6/',
+        Locale::KEY_ICON_FRENCH => 'Moustache7/',
+        Locale::KEY_ICON_HOOK => 'Moustache8/',
+        Locale::KEY_ICON_BIKEHANDLE => 'Moustache9/'
     }
     SIDEBURNS = {
-        'aucuns': 'None/',
-        'prépubaires': 'Sideburn1/',
-        'prépubaires longue': 'Sideburn2/',
-        'début de pousse': 'Sideburn3/',
-        'fines': 'Sideburn4/',
-        'coupées court': 'Sideburn9/',
-        'pointes': 'Sideburn5/',
-        'moyennes': 'Sideburn6/',
-        'large': 'Sideburn7/',
-        'longues': 'Sideburn8/',
-        'complètes': 'Sideburn10/',
-        'complètes longues': 'Sideburn11/'
+        Locale::KEY_ICON_NONES_M => 'None/',
+        Locale::KEY_ICON_PREPUBESCENT => 'Sideburn1/',
+        Locale::KEY_ICON_LONG_PREPUBESCENT => 'Sideburn2/',
+        Locale::KEY_ICON_START_GROWTH => 'Sideburn3/',
+        Locale::KEY_ICON_THINS => 'Sideburn4/',
+        Locale::KEY_ICON_SHORT_CUT => 'Sideburn9/',
+        Locale::KEY_ICON_POINTY_SIDEBURN => 'Sideburn5/',
+        Locale::KEY_ICON_MEDIUMS => 'Sideburn6/',
+        Locale::KEY_ICON_LARGE => 'Sideburn7/',
+        Locale::KEY_ICON_LONG_SIDEBURNS => 'Sideburn8/',
+        Locale::KEY_ICON_COMPLETES => 'Sideburn10/',
+        Locale::KEY_ICON_LONG_COMPLETES => 'Sideburn11/'
     }
     HAIRSTYLES_FRONT = {
-        'chauve': 'Bald/',
-        'calvicie': 'Hair16/',
-        'rasé': 'Hair17/',
-        'plaqués': 'Hair14/',
-        'attachés': 'Hair6/',
-        'court': 'Hair1/',
-        'pointes': 'Hair13/',
-        'frange 1': 'Hair2/',
-        'frange 2': 'Hair3/',
-        'frange lisse': 'Hair4/',
-        'frange rideau courte': 'Hair5/',
-        'frange rideau': 'Hair12/',
-        'couvre-yeux': 'Hair7/',
-        'couvre-oeuil': 'Hair8/',
-        'meches droites': 'Hair9/',
-        'meches diagonales': 'Hair10/',
-        'meches larges': 'Hair11/',
-        'crête': 'Hair15/'
+        Locale::KEY_ICON_BALD => 'Bald/',
+        Locale::KEY_ICON_RECEEDING => 'Hair16/',
+        Locale::KEY_ICON_SHAVED => 'Hair17/',
+        Locale::KEY_ICON_FLAT => 'Hair14/',
+        Locale::KEY_ICON_ATTACHED => 'Hair6/',
+        Locale::KEY_ICON_SHORT_M => 'Hair1/',
+        Locale::KEY_ICON_SPIKY => 'Hair13/',
+        Locale::KEY_ICON_FRINGE1 => 'Hair2/',
+        Locale::KEY_ICON_FRINGE2 => 'Hair3/',
+        Locale::KEY_ICON_FLAT_FRINGE => 'Hair4/',
+        Locale::KEY_ICON_SHORT_BLIND_FRINGE => 'Hair5/',
+        Locale::KEY_ICON_BLIND_FRINGE => 'Hair12/',
+        Locale::KEY_ICON_OVER_EYES => 'Hair7/',
+        Locale::KEY_ICON_EYE_COVER => 'Hair8/',
+        Locale::KEY_ICON_STRAIGHT_STRANDS => 'Hair9/',
+        Locale::KEY_ICON_DIAGONALS_STRANDS => 'Hair10/',
+        Locale::KEY_ICON_LARGE_STRANDS => 'Hair11/',
+        Locale::KEY_ICON_MOHAWK => 'Hair15/'
     }
     HAIRSTYLES_BACK = {
-        'aucun': 'Bald/',
-        'très court': 'Hair8/',
-        'court': 'Hair9/',
-        'carré': 'Hair5/',
-        'carré long dégradé': 'Hair1/',
-        'cheveux longs lisses': 'Hair2/',
-        'cheveux longs épais': 'Hair7/',
-        'queue de cheval': 'Hair3/',
-        'couettes': 'Hair4/',
-        'chignon': 'Hair6/'
+        Locale::KEY_ICON_NONE_M => 'Bald/',
+        Locale::KEY_ICON_VERY_SHORT_M => 'Hair8/',
+        Locale::KEY_ICON_SHORT_M => 'Hair9/',
+        Locale::KEY_ICON_SQUARE => 'Hair5/',
+        Locale::KEY_ICON_LONG_SQUARE => 'Hair1/',
+        Locale::KEY_ICON_LONG_FLAT => 'Hair2/',
+        Locale::KEY_ICON_LONG_PUFFY => 'Hair7/',
+        Locale::KEY_ICON_PONYTAIL => 'Hair3/',
+        Locale::KEY_ICON_PIGTAILS => 'Hair4/',
+        Locale::KEY_ICON_BUN => 'Hair6/'
     }
     HAIR_COLORS = {
-        'blanc': 'white',
-        'clair': 'light',
-        'sombre': 'half',
-        'noirs': 'dark'
+        Locale::KEY_ICON_WHITE => 'white',
+        Locale::KEY_ICON_LIGHT => 'light',
+        Locale::KEY_ICON_DARK => 'half',
+        Locale::KEY_ICON_BLACKS => 'dark'
     }
 
     def initialize
-        @skintone = 'pale'
-        @corpulence = 'moyenne'
-        @ears = 'moyennes'
-        @brows = 'moyens'
+        @skintone = Locale::KEY_ICON_PALE
+        @corpulence = Locale::KEY_ICON_MEDIUM
+        @ears = Locale::KEY_ICON_MEDIUMS
+        @brows = Locale::KEY_ICON_MEDIUMS_M
         @eyes = 'Eyes1/'
-        @eye_color = 'sombre'
-        @nose = 'moyen'
+        @eye_color = Locale::KEY_ICON_DARK
+        @nose = Locale::KEY_ICON_MEDIUM_M
         @mouth = 'mouth1'
-        @hairstyle_front = 'court'
-        @hairstyle_back = 'aucun'
-        @hair_color = 'clair'
-        @beard_color = 'clair'
-        @beard = 'aucune'
-        @moustache = 'aucune'
-        @sideburns = 'aucuns'
+        @hairstyle_front = Locale::KEY_ICON_SHORT_M
+        @hairstyle_back = Locale::KEY_ICON_NONE_M
+        @hair_color = Locale::KEY_ICON_LIGHT
+        @beard_color = Locale::KEY_ICON_LIGHT
+        @beard = Locale::KEY_ICON_NONE
+        @moustache = Locale::KEY_ICON_NONE
+        @sideburns = Locale::KEY_ICON_NONES_M
     end
 
     def get_picture
         picture = setup_picture(BACKGROUND)
-        picture.superpose(setup_picture(HAIRSTYLES_FOLDER+BACK_FOLDER+HAIRSTYLES_BACK[@hairstyle_back.to_sym]+HAIR_COLORS[@hair_color.to_sym]))
-        picture.superpose(setup_picture(TONES[@skintone.to_sym]+EARS_FOLDER+EARS[@ears.to_sym]))
-        picture.superpose(setup_picture(TONES[@skintone.to_sym]+BASE_FOLDER+SIZES[@corpulence.to_sym]))
-        picture.superpose(setup_picture(TONES[@skintone.to_sym]+BROWS_FOLDER+BROWS[@brows.to_sym]))
-        picture.superpose(setup_picture(TONES[@skintone.to_sym]+EYES_FOLDER+@eyes+EYES_COLOR[@eye_color.to_sym]))
-        picture.superpose(setup_picture(TONES[@skintone.to_sym]+NOSES_FOLDER+NOSES[@nose.to_sym]))
-        picture.superpose(setup_picture(TONES[@skintone.to_sym]+MOUTHS_FOLDER+@mouth))
-        picture.superpose(setup_picture(BEARDS_FOLDER+SIDEBURNS_FOLDER+SIDEBURNS[@sideburns.to_sym]+HAIR_COLORS[@beard_color.to_sym]))
-        picture.superpose(setup_picture(BEARDS_FOLDER+CHIN_FOLDER+BEARDS[@beard.to_sym]+HAIR_COLORS[@beard_color.to_sym]))
-        picture.superpose(setup_picture(BEARDS_FOLDER+MOUSTACHE_FOLDER+MOUSTACHES[@moustache.to_sym]+HAIR_COLORS[@beard_color.to_sym]))
-        picture.superpose(setup_picture(HAIRSTYLES_FOLDER+FRONT_FOLDER+HAIRSTYLES_FRONT[@hairstyle_front.to_sym]+HAIR_COLORS[@hair_color.to_sym]))
+        picture.superpose(setup_picture(HAIRSTYLES_FOLDER+BACK_FOLDER+HAIRSTYLES_BACK[@hairstyle_back.to_sym].to_s+HAIR_COLORS[@hair_color.to_sym].to_s))
+        picture.superpose(setup_picture(TONES[@skintone.to_sym].to_s+EARS_FOLDER+EARS[@ears.to_sym].to_s))
+        picture.superpose(setup_picture(TONES[@skintone.to_sym].to_s+BASE_FOLDER+SIZES[@corpulence.to_sym].to_s))
+        picture.superpose(setup_picture(TONES[@skintone.to_sym].to_s+BROWS_FOLDER+BROWS[@brows.to_sym].to_s))
+        picture.superpose(setup_picture(TONES[@skintone.to_sym].to_s+EYES_FOLDER+@eyes+EYES_COLOR[@eye_color.to_sym].to_s))
+        picture.superpose(setup_picture(TONES[@skintone.to_sym].to_s+NOSES_FOLDER+NOSES[@nose.to_sym].to_s))
+        picture.superpose(setup_picture(TONES[@skintone.to_sym].to_s+MOUTHS_FOLDER+@mouth))
+        picture.superpose(setup_picture(BEARDS_FOLDER+SIDEBURNS_FOLDER+SIDEBURNS[@sideburns.to_sym].to_s+HAIR_COLORS[@beard_color.to_sym].to_s))
+        picture.superpose(setup_picture(BEARDS_FOLDER+CHIN_FOLDER+BEARDS[@beard.to_sym].to_s+HAIR_COLORS[@beard_color.to_sym].to_s))
+        picture.superpose(setup_picture(BEARDS_FOLDER+MOUSTACHE_FOLDER+MOUSTACHES[@moustache.to_sym].to_s+HAIR_COLORS[@beard_color.to_sym].to_s))
+        picture.superpose(setup_picture(HAIRSTYLES_FOLDER+FRONT_FOLDER+HAIRSTYLES_FRONT[@hairstyle_front.to_sym].to_s+HAIR_COLORS[@hair_color.to_sym].to_s))
         return picture
     end
 
@@ -303,7 +303,7 @@ class PlayerIcon
             elements,
             -> (element) {
                 if (element != Narrator::RETURN_BUTTON)
-                    return element
+                    return Locale::get_localized(element)
                 else
                     return return_label + ' ' + Locale.get_localized(current)
                 end
