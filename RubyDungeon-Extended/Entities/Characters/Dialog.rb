aa3ce0658fe1f29b925e967559beb745e681e299
Alice Mailhes <armand.mailhes@gmail.com>
2024-12-24 11:47:39 +0100

    WIP implementing dialog system

--- RubyDungeon-Extended/Entities/Characters/Dialog.rb
+++ RubyDungeon-Extended/Entities/Characters/Dialog.rb
@@ -1,16 +1,23 @@
 class Dialog
     NO_LEVEL_REQ = nil
     NO_ITEMS_REQ = nil
+    NO_INTRO = nil
 
-    def initialize(sentences, trigger_words, min_level = NO_LEVEL_REQ, max_level = NO_LEVEL_REQ, required_items = NO_ITEMS_REQ)
-        @sentences = sentences
+    def initialize(id, trigger_words, sentences, intro = NO_INTRO, min_level = NO_LEVEL_REQ, max_level = NO_LEVEL_REQ, required_items = NO_ITEMS_REQ)
         @trigger_words = trigger_words
+        @sentences = sentences
+        @intro = intro
         @min_level = min_level
         @max_level = max_level
         @required_items = required_items
     end
 
-    def triggered?(sentence_heard)
+    def self.process_sentence(sentence_heard)
+        return processed_sentence = sentence_heard.gsub("'", " ").downcase.split
+    end
+
+    def triggered?(processed_sentence)
         # test if trigger words in sentence heard, see how to test for vaguely ressembling string
+        # if repeated, replace @intro by "As I said,"
     end
 end
--- RubyDungeon-Extended/Enums/Characters/ShopKeeper.rb
+++ RubyDungeon-Extended/Enums/Characters/ShopKeeper.rb
@@ -2,7 +2,42 @@ module ShopKeeper
     NAME = Locale::KEY_SHOPKEEPER_NAME
     INTRO_DIALOG = Locale::KEY_SHOPKEEPER_INTRO_DIALOG
     IDLE_DIALOGS = Locale::KEY_SHOPKEEPER_IDLE_DIALOGS
-    DIALOGS = []
+    DIAL_ID_NAME = 'name'
+    DIAL_ID_TOO_EXPENSIVE = 'too_expensive'
+    DIAL_ID_FUCK_YOU = 'fuck_you'
+    DIAL_ID_GET_LOST = 'get_lost'
+    DIAL_ID_THREAT = 'threat'
+    DIALOGS = [
+        Dialog.new(
+            DIAL_ID_NAME,
+            ["your|you", "name|called"],
+            ["My name is Márton Orbán."]
+        ),
+        Dialog.new(
+            DIAL_ID_TOO_EXPENSIVE,
+            ["too|excessive", "expensive|much"],
+            ["I for one believe that my prices are within my ware's market value.", "But I apologies if what I sell is beyond your budget."],
+            "Is that so?\n"
+        ),
+        Dialog.new(
+            DIAL_ID_FUCK_YOU,
+            ["fuck|hate", "you|yourself"],
+            ["The adventurer's here trully have no sense of customs.\nIs it the tower's aura getting to your nerves or you just never had any education?"],
+            "How rude.\n"
+        ),
+        Dialog.new(
+            DIAL_ID_GET_LOST,
+            ["go|get|burn", "hell|lost"],
+            ["wishing much of the same to you.", "Now, if we could just focus on the buying, please.\nThis is a shop, after all."],
+            "Sure, sure, "
+        ),
+        Dialog.new(
+            DIAL_ID_THREAT,
+            ["kill|murder|hurt", "you"],
+            ["this isn't the dungeon here. Actions have consequences out there."],
+            "Oh, so we are going with threats, huh?\nMay I remind you that "
+        )
+    ]
     UNKNOWN_DIALOGS = []
     PICTURE = 'merchant'
 end
