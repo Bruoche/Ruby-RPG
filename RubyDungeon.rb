5ac610374a08a0c44d26d95c56dcdd30fb3e1101
Alice Mailhes <armand.mailhes@gmail.com>
2025-09-02 14:44:58 +0200

    Excluding the dialog generator from ruby dungeon's loaded files

--- RubyDungeon.rb
+++ RubyDungeon.rb
@@ -1,25 +1,39 @@
 $:.unshift File.dirname($0)
 puts "Loading..."
+
+EXCLUDED = [
+    "RubyDungeon-Extended/DialogGenerator/"
+]
+
+def require_if_valid(file)
+    for excluded in EXCLUDED
+        if file.include?(excluded)
+            return
+        end
+    end
+    require(file)
+end
+
 require "RubyDungeon-Extended/ASCIIPrinter.rb"
-Dir["RubyDungeon-Extended/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/ArmorTypes/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/*/Item.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/*/Coins.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/*/Monster.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/Entities/*/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Items/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/EntryRequirements/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Monsters/Goblin.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Monsters/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Bosses/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Statuses/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Characters/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Biomes/BossCell.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Biomes/OldDungeon.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/Biomes/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/SpecialRooms/*.rb"].each {|file| require(file)}
-Dir["RubyDungeon-Extended/*/*/*.rb"].each {|file| require(file)}
+Dir["RubyDungeon-Extended/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/ArmorTypes/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/*/Item.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/*/Coins.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/*/Monster.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/Entities/*/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Items/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/EntryRequirements/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Monsters/Goblin.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Monsters/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Bosses/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Statuses/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Characters/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Biomes/BossCell.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Biomes/OldDungeon.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/Biomes/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/SpecialRooms/*.rb"].each {|file| require_if_valid(file)}
+Dir["RubyDungeon-Extended/*/*/*.rb"].each {|file| require_if_valid(file)}
 
 require "fileutils" # Manage files
 require "time" # get current time
