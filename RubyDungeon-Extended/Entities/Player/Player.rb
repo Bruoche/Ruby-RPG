dfe71c6c89fea5b1176efcb4e8dd8e61e7d13384
Alice Mailhes <armand.mailhes@gmail.com>
2025-11-02 23:32:51 +0100

    Removing unwanted tabs

--- RubyDungeon-Extended/Entities/Player/Player.rb
+++ RubyDungeon-Extended/Entities/Player/Player.rb
@@ -126,13 +126,13 @@ class Player
         if (monsters_power == 0)
             return MathUtils::HUNDRED_PERCENT
         end
-		spot_risk = (monsters_power - stealth_score)
+        spot_risk = (monsters_power - stealth_score)
         spot_risk = (spot_risk*MathUtils::HUNDRED_PERCENT)/(monsters_power)
         if spot_risk <= 0
             return MathUtils::HUNDRED_PERCENT
         end
-		return MathUtils::HUNDRED_PERCENT - spot_risk
-	end
+        return MathUtils::HUNDRED_PERCENT - spot_risk
+    end
 
     def can_escape?(monsters_power)
         perception_score = rand(monsters_power + 1)
